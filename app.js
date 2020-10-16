0a37cc7c601a5a831a46b5ea0ca5ed13c51f09eb
Duong_Dinh_Quang <duongquang16111997@gmail.com>
2020-10-16 22:06:28 +0700

    test deploy

--- app.js
+++ app.js
@@ -76,12 +76,13 @@ app.use(function (err, req, res, next) {
 
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static("client/build"));
+  // app.use(express.static("client/build"));
 
-  app.get("*", (req, res) => {
-    // res.sendFile(path.resolve(__dirname, "../client/build/index.html"));
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
+  // app.get("*", (req, res) => {
+  //   // res.sendFile(path.resolve(__dirname, "../client/build/index.html"));
+  //   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  // });
+  app.use("*", express.static(path.join(__dirname, "client", "build")));
 }
 
 // app.listen((port = 5000), function () {
