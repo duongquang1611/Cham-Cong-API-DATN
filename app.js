c44d8bcaf3f02c37376aab468791ba017efe3de3
Duong_Dinh_Quang <duongquang16111997@gmail.com>
2020-10-17 00:15:11 +0700

    test

--- app.js
+++ app.js
@@ -58,6 +58,17 @@ app.use(cookieParser());
 // app.use("/", indexRouter);
 app.use("/api/accounts", accountRouters);
 
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static(path.join(__dirname, "client", "build")));
+
+  // ...
+  // Right before your app.listen(), add this:
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
@@ -73,17 +84,6 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render("error");
 });
-
-if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  app.use(express.static(path.join(__dirname, "client", "build")));
-
-  // ...
-  // Right before your app.listen(), add this:
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-  });
-}
 
 // app.listen((port = 5000), function () {
 //   console.log("Server listening connect port " + port);
